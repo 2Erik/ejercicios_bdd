f2f4ecd1c2ec67a050edaeb62097f8dd31062592
2Erik <erik19yogo@gmail.com>
2026-05-16 14:45:39 -0500

    ERT: Selecccion de datos

--- tabla_postgresql.sql
+++ tabla_postgresql.sql
@@ -13,18 +13,28 @@ CREATE TABLE estudiantes(
 INSERT INTO estudiantes 
 (id_estudiante,nombre,apellido,edad,curso,fecha_registro) 
 VALUES
-(1,'Juan','Perez',20,'Programacion','2025-01-10'),
-(2,'Maria','Lopez',22,'Base de Datos','2025-01-12'),
-(3,'Carlos','Mora',19,'Programacion','2025-01-15'),
-(4,'Ana','Vera',21,'Redes','2025-01-18'),
-(5,'Luis','Sanchez',23,'Base de Datos','2025-01-20'),
-(6,'Sofia','Ramirez',20,'Matematicas','2025-01-22'),
-(7,'Diego','Castro',24,'Programacion','2025-01-25'),
-(8,'Valeria','Gomez',18,'Redes','2025-01-28'),
-(9,'Pedro','Herrera',22,'Matematicas','2025-02-01'),
-(10,'Camila','Torres',19,'Base de Datos','2025-02-03'),
-(11,'Jorge','Paredes',25,'Programacion','2025-02-05'),
-(12,'Lucia','Mendoza',21,'Redes','2025-02-08'),
-(13,'Andres','Silva',20,'Matematicas','2025-02-10'),
-(14,'Daniela','Cruz',23,'Base de Datos','2025-02-12'),
-(15,'Mateo','Rojas',22,'Programacion','2025-02-15');
+(1,'Juan','Perez',20,'Programacion','2026-01-10'),
+(2,'Maria','Lopez',22,'Base de Datos','2026-01-12'),
+(3,'Carlos','Mora',19,'Programacion','2026-01-15'),
+(4,'Ana','Vera',21,'Redes','2026-01-18'),
+(5,'Luis','Sanchez',23,'Base de Datos','2026-01-20'),
+(6,'Sofia','Ramirez',20,'Matematicas','2026-01-22'),
+(7,'Diego','Castro',24,'Programacion','2026-01-25'),
+(8,'Valeria','Gomez',18,'Redes','2026-01-28'),
+(9,'Pedro','Herrera',22,'Matematicas','2026-02-01'),
+(10,'Camila','Torres',19,'Base de Datos','2026-02-03'),
+(11,'Jorge','Paredes',25,'Programacion','2026-02-05'),
+(12,'Lucia','Mendoza',21,'Redes','2026-02-08'),
+(13,'Andres','Silva',20,'Matematicas','2026-02-10'),
+(14,'Daniela','Cruz',23,'Base de Datos','2026-02-12'),
+(15,'Mateo','Rojas',22,'Programacion','2026-02-15');
+
+--Consultas
+SELECT * FROM estudiantes;
+SELECT nombre, curso FROM estudiantes;
+SELECT * FROM estudiantes WHERE edad > 18;
+SELECT * FROM estudiantes WHERE edad BETWEEN 18 AND 25;
+SELECT * FROM estudiantes WHERE curso = 'Base de Datos';
+SELECT * FROM estudiantes WHERE fecha_registro > '2026-03-01';
+SELECT * FROM estudiantes WHERE fecha_registro BETWEEN '2026-01-01' and '2026-04-30';
+
